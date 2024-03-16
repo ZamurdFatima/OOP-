77e32486a257462962fa7b06243552802dbf191d
ZamurdFatima <fatimazamurd001@gmail.com>
2024-03-16 11:52:04 +0500

    Enums
    Make program more readable.

---
+++ enums.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+//Enums are used to make the program more readable.
+ int main(){
+    enum Meal{ breakfast, lunch, dinner};
+    Meal m1 = lunch;
+    cout<<m1;
+    return 0;
+}                                          //it gives seq number from 0 to so no.
+    
