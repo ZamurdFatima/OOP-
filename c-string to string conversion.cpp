ab44cbce0f8952f59fce0505d85ec022675d2415
ZamurdFatima <fatimazamurd001@gmail.com>
2024-05-05 13:25:03 +0500

    conversion
    Character-string to string and vice virsa

--- c-string to string conversion.cpp
+++ c-string to string conversion.cpp
@@ -6,30 +6,26 @@
  {
  private:
  enum{ SZ = 80 }; 
- char str[SZ]; //holds a C-string
+ char str[SZ];           //holds a C-string
  public:
  String() 
  { str[0] = '\0'; }
  String( char s[] ) 
- { strcpy(str, s); } // convert C-string to String
+ { strcpy(str, s); }     // convert C-string to String
  void display() const 
  { cout << str; }
  operator char*() 
- { return str; } }; //convert String to C-string
+ { return str; } };      //convert String to C-string
  
  int main()
  {
- String s1; 
- //create and initialize C-string
- char xstr[] = "Joyeux Noel! ";
- s1 = xstr; //use 1-arg constructor to convert C-string to String
- 
+ String s1;                         
+ char xstr[] = "Joyeux Noel! ";      //create and initialize C-string
+ s1 = xstr;                          //use 1-arg constructor to convert C-string to String
  s1.display(); 
- String s2 = "Bonne Annee!"; //uses 1-arg constructor to initialize String
-
- cout << static_cast<char*>(s2); 
-
- cout << endl; //to convert String to C-string
  
+ String s2 = "Bonne Annee!";         //uses 1-arg constructor to initialize String
+ cout << static_cast<char*>(s2);     //to convert String to C-string
+ cout << endl;             
  return 0; //before sending to << op
  }
